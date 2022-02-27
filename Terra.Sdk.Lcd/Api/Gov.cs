b7b7c7ac576529050f8eefe73e98784a613a8607
Luke Wilson <luke.wilson@intelligenthack.com>
2022-02-27 23:36:28 +0000

    Moar stubs

---
+++ Terra.Sdk.Lcd/Api/Gov.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Terra.Sdk.Lcd.Models;
+using Terra.Sdk.Lcd.Models.Entities;
+
+namespace Terra.Sdk.Lcd.Api
+{
+    public class Gov
+    {
+        private readonly LcdClient _client;
+
+        public Gov(LcdClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<PaginatedResult<Proposal>> GetProposals(string paginationKey = null, int? pageNumber = null,
+            bool? getTotalCount = null, bool? isDescending = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<Proposal>> GetProposal(long proposalId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<string>> GetProposer(long proposalId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Coin>> GetInitialDeposit(long proposalId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<PaginatedResult<Deposit>> GetDeposits(long proposalId,
+            string paginationKey = null, int? pageNumber = null, bool? getTotalCount = null, bool? isDescending = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<Tx>> SearchProposalCreationTx(long proposalId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<PaginatedResult<Vote>> GetVotes(long proposalId,
+            string paginationKey = null, int? pageNumber = null, bool? getTotalCount = null, bool? isDescending = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<Tally>> GetTally(long proposalId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<DepositParams>> GetDepositParameters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<VotingParams>> GetVotingParameters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<TallyParams>> GetTallyParameters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<GovParams>> GetGovParameters()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Terra.Sdk.Lcd/Models/Entities/Deposit.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Terra.Sdk.Lcd.Models.Entities
+{
+    public class Proposal
+    {
+        
+    }
+    public class Tx
+    {
+        
+    }
+    public class Vote
+    {
+        
+    }
+    public class Tally
+    {
+        
+    }
+    public class DepositParams
+    {
+        
+    }
+    public class VotingParams
+    {
+        
+    }
+    public class TallyParams
+    {
+        
+    }
+    public class GovParams
+    {
+        
+    }
+
+    public class Deposit
+    {
+        [JsonProperty("proposal_id")]
+        public string ProposalId { get; set; }
+
+        [JsonProperty("depositor")]
+        public string Depositor { get; set; }
+
+        [JsonProperty("amount")]
+        public List<Coin> Amount { get; set; }
+    }
+}
