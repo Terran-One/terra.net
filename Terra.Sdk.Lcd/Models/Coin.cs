bf4995d79e19a67836efcacfa2ecc896d5e1ed5d
Luke Wilson <luke.wilson@intelligenthack.com>
2022-02-27 11:07:16 +0000

    Make Coin into a struct

--- Terra.Sdk.Lcd/Models/Coin.cs
+++ Terra.Sdk.Lcd/Models/Coin.cs
@@ -2,7 +2,7 @@ using System;
 using System.Runtime.Serialization;
 using System.Security.Permissions;
 
-namespace Terra.Sdk.Lcd.Models.Entities
+namespace Terra.Sdk.Lcd.Models
 {
     [Serializable]
     public readonly struct Coin: ISerializable
