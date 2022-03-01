3238b24362012368be76763b59b08384cc63442f
Luke Wilson <luke.wilson@intelligenthack.com>
2022-03-01 13:36:58 +0000

    MsgSend

--- Terra.Sdk.Lcd/Models/Entities/Tx/Msg/BankMsg/MsgSend.cs
+++ Terra.Sdk.Lcd/Models/Entities/Tx/Msg/BankMsg/MsgSend.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+
 namespace Terra.Sdk.Lcd.Models.Entities.Tx.Msg.BankMsg
 {
     public class MsgSend
     {
-            
+        public string FromAddress { get; set; }
+        public string ToAddress { get; set; }
+        public List<Coin> Amount { get; set; }
     }
 }
