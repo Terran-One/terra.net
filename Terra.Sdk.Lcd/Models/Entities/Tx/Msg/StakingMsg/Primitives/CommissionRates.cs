b4641f9e3f21fa3a402737ec830118ca9dddc42f
Luke Wilson <luke.wilson@intelligenthack.com>
2022-03-01 23:25:43 +0000

    Class to struct

--- Terra.Sdk.Lcd/Models/Entities/Tx/Msg/StakingMsg/Primitives/CommissionRates.cs
+++ Terra.Sdk.Lcd/Models/Entities/Tx/Msg/StakingMsg/Primitives/CommissionRates.cs
@@ -1,9 +1,41 @@
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+
 namespace Terra.Sdk.Lcd.Models.Entities.Tx.Msg.StakingMsg.Primitives
 {
-    public class CommissionRates
+    public readonly struct CommissionRates : ISerializable
     {
-        public decimal Rate { get; set; }
-        public decimal MaxRate { get; set; }
-        public decimal  MaxChangeRate { get; set; }
+        public decimal Rate { get; }
+        public decimal MaxRate { get; }
+        public decimal MaxChangeRate { get; }
+
+        public CommissionRates(decimal rate, decimal maxRate, decimal maxChangeRate) : this()
+        {
+            Rate = rate;
+            MaxRate = maxRate;
+            MaxChangeRate = maxChangeRate;
+        }
+
+        /// <remarks>
+        /// Used for serialization.
+        /// </remarks>
+        public CommissionRates(SerializationInfo info, StreamingContext text) : this()
+        {
+
+            Rate = info.GetDecimal("rate");
+            MaxRate = info.GetDecimal("max_rate");
+            MaxChangeRate = info.GetDecimal("max_change_rate");
+        }
+
+        /// <remarks>
+        /// Called during serialization.
+        /// </remarks>
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("rate", Rate);
+            info.AddValue("max_rate", MaxRate);
+            info.AddValue("max_change_rate", MaxChangeRate);
+        }
     }
 }
