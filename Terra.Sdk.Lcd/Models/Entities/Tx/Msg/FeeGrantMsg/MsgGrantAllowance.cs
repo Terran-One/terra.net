dd2de3cf1a231a536238753df55722997857f28f
Luke Wilson <luke.wilson@intelligenthack.com>
2022-03-01 16:55:13 +0000

    FeeGrantMsg

--- Terra.Sdk.Lcd/Models/Entities/Tx/Msg/FeeGrantMsg/MsgGrantAllowance.cs
+++ Terra.Sdk.Lcd/Models/Entities/Tx/Msg/FeeGrantMsg/MsgGrantAllowance.cs
@@ -2,6 +2,8 @@ namespace Terra.Sdk.Lcd.Models.Entities.Tx.Msg.FeeGrantMsg
 {
     public class MsgGrantAllowance : Msg
     {
-
+        public string Granter { get; set; }
+        public string Grantee { get; set; }
+        public Models.Entities.Allowance.Allowance Allowance { get; set; }
     }
 }
--- Terra.Sdk.Lcd/Models/Entities/Tx/Msg/FeeGrantMsg/MsgRevokeAllowance.cs
+++ Terra.Sdk.Lcd/Models/Entities/Tx/Msg/FeeGrantMsg/MsgRevokeAllowance.cs
@@ -2,6 +2,7 @@ namespace Terra.Sdk.Lcd.Models.Entities.Tx.Msg.FeeGrantMsg
 {
     public class MsgRevokeAllowance : Msg
     {
-
+        public string Granter { get; set; }
+        public string Grantee { get; set; }
     }
 }
