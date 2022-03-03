c0ee55a7a9c342c37214bfa896fd3b759811f514
Luke Wilson <luke.wilson@intelligenthack.com>
2022-03-03 12:34:42 +0000

    Remove dead code

--- Terra.Sdk.Lcd/Models/Entities/Authorization/Authorization.cs
+++ Terra.Sdk.Lcd/Models/Entities/Authorization/Authorization.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Threading.Tasks;
 using JsonSubTypes;
 using Newtonsoft.Json;
 
@@ -11,25 +9,7 @@ namespace Terra.Sdk.Lcd.Models.Entities.Authorization
     [JsonSubtypes.KnownSubType(typeof(StakeAuthorization), "/cosmos.staking.v1beta1.StakeAuthorization")]
     public class Authorization
     {
-        private readonly LcdClient _client;
-
         [JsonProperty("@type")]
         public string Type { get; set; }
-
-        public Authorization()
-        {
-        }
-
-        internal Authorization(LcdClient client)
-        {
-            _client = client;
-        }
-
-        internal Task<PaginatedResult<Authorization>> Get(
-            string granter, string grantee, string msgTypeUrl = null,
-            string paginationKey = null, int? pageNumber = null, bool? getTotalCount = null, bool? isDescending = null)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
